24d1778f4f18d353244395b8f79fcb29bfd19fae
rahulTalreja7 <rahultalreja667336@gmail.com>
2016-02-03 06:20:59 -0800

    tells whether num is prime or not

--- isPrime.java
+++ isPrime.java
@@ -1,19 +1,22 @@
 public class isPrime {
    public static void main (String [] args)
     {
+       boolean isPrime=true;
       int num=Integer.parseInt(args[0]);
        System.out.println("you have entered "+ num);
-       for (int i=2;i<num;i++){
+       for (int i=2;i<num/2;i++){
            if(num%i==0){
-               System.out.println(args[0]+" is not a prime number");
-               break;
+               isPrime=false;
            }
-           else{
-               System.out.println(args[0]+" is a prime number");
-               break;
-           }
-           
+
        }
+       if(isPrime)
+           System.out.println("number is prime");
+       else
+           System.out.println("not a prime number");
+       
+           
+       
      }
          
      }
