c1506f2bbf756b12654c5083528ef6e56faa9f16
rahulTalreja7 <rahultalreja667336@gmail.com>
2016-03-17 14:03:15 -0700

    simple 2x2 Matrix Subraction

---
+++ MatrixSub.java
@@ -0,0 +1,23 @@
+public class MatrixSub{
+	
+	public static void main (String[] args){
+		int [][] a= {{1,2},{3,4}};
+		int [][] b= {{4,6},{7,8}};
+		int [][] c= new int[2][2];
+		
+		for (int i=0;i<a.length;i++){
+			for(int j=0;j<b.length;j++){
+				c[i][j]=a[i][j]-b[i][j];
+			}
+		}
+		
+		//printing c
+		for (int i=0;i<a.length;i++){
+			for(int j=0;j<b.length;j++){
+				System.out.printf ("%-3d ", c[i][j]);
+			}
+			System.out.println();
+		}
+		
+	}
+}
